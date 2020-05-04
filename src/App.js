a44b4a72305254e42c8e3e734330ed760ae7e2bf
Emin <eminaydin93@yahoo.com>
2020-05-04 16:45:00 +0200

    Semantic-ui added, styles chang3ed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import Countdown from "./components/countdown/countdown"
 import Reveal from "./components/reveal/reveal"
-import photo from  "./photo-1587802480811-4be6efcdf373.jpeg";
+import photo from "./photo-1587802480811-4be6efcdf373.jpeg";
+
 
 class App extends React.Component {
   constructor(props) {
@@ -13,38 +14,38 @@ class App extends React.Component {
       timeLeft: true,
     }
   }
-  
+
   timeCalculator = () => {
     this.setState({
       timeLeft: false,
     })
   }
   render() {
-   
+
 
     return (
-      
+
       <div className="App">
 
         {this.state.timeLeft
           ?
 
-          <div className="main-wrapper"> 
-          <Countdown
-            deadline={this.state.deadline}
-            picture={this.state.picture}
-            newDeadline={this.state.newDeadline}
-            parentFunc={this.timeCalculator}
-            photo = {photo}
-            theme = "light"
-          />
+          <div className="main-wrapper">
+            <Countdown
+              deadline={this.state.deadline}
+              picture={this.state.picture}
+              newDeadline={this.state.newDeadline}
+              parentFunc={this.timeCalculator}
+              photo={photo}
+              theme="light"
+            />
 
           </div>
           :
-         <Reveal/>}
+          <Reveal />}
 
       </div>
-      
+
     );
   }
 
--- src/components/countdown/countdown.js
+++ src/components/countdown/countdown.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "./countdown.css"
-
-
-
+import {
+    Button,
+    Input
+} from 'semantic-ui-react';
 class Countdown extends React.Component {
 
     constructor(props) {
@@ -13,12 +14,7 @@ class Countdown extends React.Component {
             minutes: 0,
             seconds: 0,
             deadline: this.props.deadline,
-           
-
         }
-     
-
-
     }
 
 
@@ -31,25 +27,25 @@ class Countdown extends React.Component {
         console.log(
             "hello"
         );
-        
+
     }
 
     calculateTheTime(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
         if (time > 0) {
-        const seconds = Math.floor((time / 1000) % 60);
-        const minutes = Math.floor((time / 1000 / 60) % 60);
-        const hours = Math.floor(time / (1000 * 60 * 60) % 24);
-        const days = Math.floor(time / (1000 * 60 * 60 * 24));
-        this.setState({
-            days,
-            hours,
-            minutes,
-            seconds,
-        })
-     }
-        else {this.props.parentFunc();}
-        
+            const seconds = Math.floor((time / 1000) % 60);
+            const minutes = Math.floor((time / 1000 / 60) % 60);
+            const hours = Math.floor(time / (1000 * 60 * 60) % 24);
+            const days = Math.floor(time / (1000 * 60 * 60 * 24));
+            this.setState({
+                days,
+                hours,
+                minutes,
+                seconds,
+            })
+        }
+        else { this.props.parentFunc(); }
+
 
     }
     changeDeadline() {
@@ -65,34 +61,36 @@ class Countdown extends React.Component {
 
         return (
             <div className="countdown">
-                <div className={`left-divider ${this.props.theme === "dark" ? "dark" : ""}`} style = {this.props.photo ? (
-                {
-                    backgroundImage: "url(" + this.props.photo + ")",
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat'
-                }
-            ) :
-             {
-               
-            }}> 
-                <div className="info">
-                    <div>Countdown to {this.state.deadline}</div>
-                    <div><span>{this.state.days}</span> days</div>
-                    <div><span> {this.state.hours}</span> hours</div>
-                    <div><span> {this.state.minutes}</span> minutes</div>
-                    <div><span> {this.state.seconds}</span> seconds</div>
-                    <input placeholder="Start your own countdown"
-                    onChange= {event => this.setState({newDeadline: event.target.value})}/>
-                    <button onClick={() => this.changeDeadline()}> Submit</button>
-                </div>
+                <div className={`left-divider ${this.props.theme === "dark" ? "dark" : ""}`} style={this.props.photo ? (
+                    {
+                        backgroundImage: "url(" + this.props.photo + ")",
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }
+                ) :
+                    {
+
+                    }}>
+                    <div className="info">
+                        <div>Countdown to {this.state.deadline}</div>
+                        <div><span>{this.state.days}</span> days</div>
+                        <div><span> {this.state.hours}</span> hours</div>
+                        <div><span> {this.state.minutes}</span> minutes</div>
+                        <div><span> {this.state.seconds}</span> seconds</div>
+                        <Input placeholder="Start your own countdown"
+                            onChange={event => this.setState({ newDeadline: event.target.value })} />
+                        <br />
+                        <Button onClick={() => this.changeDeadline()}> Submit</Button>
+                    </div>
                 </div>
 
                 <div className="message-container">
 
                     <h1>{this.props.header}</h1>
-              <p>{this.props.text}</p>
-        </div>
+                    <p>{this.props.text}</p>
+                </div>
+
             </div>
         )
     }
